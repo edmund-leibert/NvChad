7c1bed4e0302ef4a4c32fff09d2a1646740b7711
Edmund Leibert III <eleibert@ucsd.edu>
2023-04-19 12:01:02 -0700

    🔨 dev[create-links.ps1]: Created a PowerShell script to automate creation of symbolic links to nvim settings folder
    • Note that script deletes all configuration files currently in the nvim
    settings folder before creating new symbolic links.

--- create-links.ps1
+++ create-links.ps1
@@ -1,5 +1,8 @@
 Write-Host "Running the script: $($MyInvocation.MyCommand.Name)"
 
+# Delete all files currently in the nvim folder
+Remove-Item -Path "C:\\Users\\edmun\\AppData\\Local\\nvim\\*" -Recurse -Force
+
 # Get the current directory
 $currentDirectory = Get-Location
 
@@ -8,9 +11,9 @@ $CONFIG_ITEMS = Get-ChildItem -Path *
 
 foreach ($CONFIG_ITEM in $CONFIG_ITEMS) {
 	Write-Host "Creating a symbolic for the following files... $($CONFIG_ITEM.name)"
-	
+
 	# If object is the file init.lua
-	if($CONFIG_ITEM.Name -eq "init.lua") { 
+	if($CONFIG_ITEM.Name -eq "init.lua") {
 		New-Item -ItemType SymbolicLink -Path "C:\\Users\\edmun\\AppData\\Local\\nvim\\$($CONFIG_ITEM.name)" -Target "$($currentDirectory.Path)\\$($CONFIG_ITEM.name)"
 	}
 	if($CONFIG_ITEM.PSIsContainer -AND $CONFIG_ITEM.Name -eq "lua") {
